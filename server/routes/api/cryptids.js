00363f415f1921fa411e5274c6c11e4493081399
HollywoodOKC <T_Lane11@yahoo.com>
2024-01-05 04:28:07 -0600

    Added field.

--- server/routes/api/cryptids.js
+++ server/routes/api/cryptids.js
@@ -56,4 +56,10 @@ router.delete('/:id', (req, res) => {
         .catch(err => res.status(404).json({ error: 'No such Cryptid!' }));
 });
 
+router.get('/:id', (req, res) => {
+    Cryptid.findById(req.params.id)
+    .then(cryptids => res.json( cryptidOf_month = !COM.cryptidOf_month))
+    .catch(err => res.status(404).json({ error: `Can't update Cryptid of the Month`}));
+});
+
 module.exports = router;

9339c1e1d54f60caf7f39c820cfb7bd8b5630dfe
HollywoodOKC <T_Lane11@yahoo.com>
2024-03-07 02:49:34 -0600

    chore:Made app work on network

--- server/app.js
+++ server/app.js
@@ -17,4 +17,4 @@ app.use('/api/cryptids', cryptids);
 
 const PORT = process.env.PORT || 8082;
 
-app.listen(PORT, () => console.log(`Server running on Port: ${PORT}`));
+app.listen(PORT, '0.0.0.0', () => console.log(`Server running on Port: ${PORT}`));
